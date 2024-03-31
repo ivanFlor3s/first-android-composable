0b60afe1baefd20f87e34473323b9c0ca5c92678
ivanFlores <ifloresmanc96@gmail.com>
2024-03-31 20:36:20 -0300

    add modifiers

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -4,12 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,14 +31,15 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyText(text: String){
+fun MyText(text: String) {
     Text(text)
 }
 
 @Composable
-fun MyTexts(){
-    Column {
+fun MyTexts() {
+    Column(modifier = Modifier.padding(start = 8.dp)) {
         MyText("Hola como andas?")
+        Spacer(modifier = Modifier.height(16.dp))
         MyText("Hello, World!")
     }
 }
@@ -40,17 +51,20 @@ fun PreviewTexts() {
 }
 
 @Composable
-fun MyComponent(){
-    Row {
+fun MyComponent() {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
         MyImage()
         MyTexts()
     }
 }
 
 @Composable
-fun MyImage(){
+fun MyImage() {
     Image(
         painter = painterResource(id = R.drawable.ic_launcher_foreground),
-        contentDescription = "Icon"
+        contentDescription = "Icon",
+        modifier = Modifier.clip(CircleShape)
+            .background(Color.Cyan)
+            .size(64.dp)
     )
 }
